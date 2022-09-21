8908b35d8e44af72f887d7ad89f8a883e2106556
Gregory Manley <gregory.f.manley@gmail.com>
2022-09-20 18:26:48 -0600

    Update UnitDensity.swift
    Fixed baseUnit override

--- Code/UnitDensity.swift
+++ Code/UnitDensity.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public class UnitDensity: Dimension {
+public final class UnitDensity: Dimension {
    static let gramPerCentimeterCubed = UnitDensity(symbol: "g/cm\u{00B3}", converter: UnitConverterLinear(coefficient: 1))
    static let gramPerMeterCubed = UnitDensity(symbol: "g/m\u{00B3}", converter: UnitConverterLinear(coefficient: 1000000))
 
@@ -12,5 +12,7 @@ public class UnitDensity: Dimension {
    static let kilogramPerLiter = UnitDensity(symbol: "kg/L", converter: UnitConverterLinear(coefficient: 1))
 
    // The SI unit for density is kg / (m^3)
-   static let baseUnit = UnitDensity.kilogramPerMeterCubed
+   public override class func baseUnit() -> UnitDensity {
+      return kilogramPerMeterCubed
+   }
 }
